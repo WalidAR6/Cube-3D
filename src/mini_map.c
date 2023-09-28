faf8d19fcda3e1051241bbfe39e8000db1f6bf89
Walid Araissi <waraissi@e3r2p12.1337.ma>
2023-09-28 19:56:08 +0100

    comment bonus functions

--- src/mini_map.c
+++ src/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: waraissi <waraissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 23:01:06 by waraissi          #+#    #+#             */
-/*   Updated: 2023/09/26 22:03:13 by waraissi         ###   ########.fr       */
+/*   Updated: 2023/09/28 16:36:48 by waraissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ void    draw_mini_map(t_win *vars, t_m_map *m_map)
 {
     int i;
     int j;
-    int x = vars->player->x_player + 120;
-    int y = vars->player->y_player - 120;
-
+    int x;
+    int y;
+    
+    y = vars->player->y_player - 120;
     i = m_map->start_x;
     while (i < MAP_WIDTH - 10)
     {
@@ -27,7 +28,7 @@ void    draw_mini_map(t_win *vars, t_m_map *m_map)
         while (j < MAP_HEIGHT - 10)
         {
             if (x < 0 || y < 0 || x / 50 > (int)ft_strlen(vars->map[0]) || y / 50 > 13)
-                my_mlx_pixel_put(vars->data, i, j, PLAYER_COLOR);
+                my_mlx_pixel_put(vars->data, i, j, GROUND_COLOR);
             else if (vars->map[y / 50][x / 50] == '1' || vars->map[y / 50][x / 50] == ' ')            
                 my_mlx_pixel_put(vars->data, i, j, PLAYER_COLOR);
             else
